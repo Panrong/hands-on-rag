360ed826faec09a18ebdad4b41e37774e0a5687d
panrong <tongpanrong@hotmail.com>
2024-06-30 22:03:33 +0800

    add naive_rag

--- utils_fn/helpers.py
+++ utils_fn/helpers.py
@@ -1,18 +1,11 @@
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
+class colors:
+    green = "\033[92m"
+    reset = "\033[0m"
 
 
-def print_qa(query: str, response_no_rag: str, response_rag) -> None:
-    text = f"{bcolors.OKGREEN}Question:{bcolors.ENDC} {query} \n" + \
-    f"{bcolors.OKGREEN}LLM:{bcolors.ENDC} {response_no_rag} \n" + \
-    f"{bcolors.OKGREEN}RAG:{bcolors.ENDC} {response_rag}"
-    print(text)
+def print_qa(query: str, response_no_rag: str, response_rag) -> None:    
+    print(f"{colors.green}Question:\n{colors.reset}{query}", end="\n\n")
+    print(f"{colors.green}LLM:\n{colors.reset}{response_no_rag}", end="\n\n")
+    print(f"{colors.green}RAG:\n{colors.reset}{response_rag}")
